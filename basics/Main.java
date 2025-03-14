464c064710ce9dd5bd1eb27abf7f7d5602d456a5
Axole Maranjana <AxoleMaranjana4@gmail.com>
2025-03-14 14:36:38 +0200

    init

---
+++ basics/Main.java
@@ -0,0 +1,22 @@
+public class Main{
+	public static void main(String[] args){
+		
+		System.out.println("Hello Main");
+
+
+		add(10, 20);
+	}
+	
+	/**
+	 * This method adds two numbrs yiu pass it and pring them in the console
+	 *@param a This is the first number
+	 *@param b This is a second number
+	 *
+	 *
+	 * @author Axole Maranjana
+	 * */
+	static void add(int a, int b){
+		System.out.print("Sum of " + a + " + " + b + " is " + (a+b));
+	}
+
+}
